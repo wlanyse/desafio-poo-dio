590d570c4f85c1bbe2ab3cc0a1f0ddd7508dbaee
wlanyse <wlanybeckman@hotmail.com>
2025-08-07 19:38:28 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+    }
+}
